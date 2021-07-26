d5ba6f8ec10f2183ad3a05577e9978ca8a9947f9
sid <dengesidrenil@gmail.com>
2021-07-26 14:42:57 +0300

    init

--- app/src/main/java/com/example/utku/MapsActivity.kt
+++ app/src/main/java/com/example/utku/MapsActivity.kt
@@ -1,10 +1,14 @@
 package com.example.utku
 
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,16 +19,20 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.example.utku.databinding.ActivityMapsBinding
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.Marker
+import java.util.*
+import java.util.function.DoubleUnaryOperator
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var currentLocation: Location
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    var currentMarker: Marker? = null
+    var currentLocation: Location? = null
+    var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,16 +41,109 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocationPermission()
     }
+
+
+    private fun getLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1000
+            )
+            return
+        }
+
+        val task = fusedLocationProviderClient?.lastLocation
+        task?.addOnSuccessListener { location ->
+            if (location != null) {
+                this.currentLocation = location
+                val mapFragment = supportFragmentManager
+                    .findFragmentById(R.id.map) as SupportMapFragment
+                mapFragment.getMapAsync(this)
+
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocationPermission()
+            }
+        }
+    }
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latlong = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
+        drawMarker(latlong)
+
+        mMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+            override fun onMarkerDragStart(p0: Marker) {
+
+            }
+
+            override fun onMarkerDrag(p0: Marker) {
+
+            }
+
+            override fun onMarkerDragEnd(p0: Marker) {
+
+                if (currentMarker != null)
+                    currentMarker?.remove()
+
+                val newLatLng = LatLng(p0.position.latitude, p0.position.latitude)
+                drawMarker(newLatLng)
+
+            }
+        })
+        /*
+               val sydney = LatLng(-34.0, 151.0)
+               mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+               mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+           }
+         */
+
+
     }
+
+    private fun drawMarker(latlong: LatLng) {
+        val markerOption = MarkerOptions().position(latlong).title("buradasın")
+            //.snippet(getAddress(latlong.latitude, latlong.latitude).toString()).draggable(true)
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latlong))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlong, 15f))
+        currentMarker = mMap.addMarker(markerOption)
+        currentMarker?.showInfoWindow()
+    }
+
+    /*
+    private fun getAddress(lat: Double, lon: Double): String? {
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        val addresses = geoCoder.getFromLocation(lat, lon, 1)
+        return addresses[0].getAddressLine(0)
+    }
+
+     */
 }
