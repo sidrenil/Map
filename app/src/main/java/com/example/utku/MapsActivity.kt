ee9513fe4b034da873335cf9c10783c196d649fa
sid <dengesidrenil@gmail.com>
2021-07-27 14:00:10 +0300

    init

--- app/src/main/java/com/example/utku/MapsActivity.kt
+++ app/src/main/java/com/example/utku/MapsActivity.kt
@@ -7,6 +7,8 @@ import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -14,23 +16,26 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.utku.databinding.ActivityMapsBinding
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.*
 import java.util.*
 import java.util.function.DoubleUnaryOperator
+import kotlin.collections.ArrayList
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, SeekBar.OnSeekBarChangeListener {
 
     var currentMarker: Marker? = null
     var currentLocation: Location? = null
     var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
 
+    private lateinit var latLngList: List<LatLng>
+    private lateinit var markerList: List<Marker>
+    private lateinit var polyLineOptions: PolylineOptions
+    var polyLine: Polyline? = null
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
@@ -41,11 +46,43 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         getLocationPermission()
-    }
 
+
+        binding.buttonDraw.setOnClickListener {
+            if (polyLine != null) polyLine!!.remove()
+
+            polyLineOptions.addAll(latLngList).clickable(true)
+            polyLine = mMap.addPolyline(polyLineOptions)
+        }
+
+        binding.buttonClear.setOnClickListener {
+            if (polyLine != null) polyLine!!.remove()
+            for (marker in markerList) {
+                marker.remove()
+            }
+
+            binding.seekWidth.setProgress(2)
+            binding.seekBlue.setProgress(0)
+            binding.seekOrange.setProgress(0)
+            binding.seekYellow.setProgress(0)
+        }
+        latLngList = ArrayList()
+        (latLngList as ArrayList<LatLng>).clear()
+
+        markerList = ArrayList()
+        (markerList as ArrayList<Marker>).clear()
+
+        binding.seekYellow.setOnSeekBarChangeListener(this)
+        binding.seekBlue.setOnSeekBarChangeListener(this)
+        binding.seekOrange.setOnSeekBarChangeListener(this)
+    }
 
     private fun getLocationPermission() {
         if (ActivityCompat.checkSelfPermission(
@@ -96,16 +133,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val latlong = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
-        drawMarker(latlong)
+        // val latlong = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
+        // drawMarker(latlong)
 
         mMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
             override fun onMarkerDragStart(p0: Marker) {
-
             }
 
             override fun onMarkerDrag(p0: Marker) {
-
             }
 
             override fun onMarkerDragEnd(p0: Marker) {
@@ -130,7 +165,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun drawMarker(latlong: LatLng) {
         val markerOption = MarkerOptions().position(latlong).title("buradasın")
-            //.snippet(getAddress(latlong.latitude, latlong.latitude).toString()).draggable(true)
+        //.snippet(getAddress(latlong.latitude, latlong.latitude).toString()).draggable(true)
 
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latlong))
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlong, 15f))
@@ -138,12 +173,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         currentMarker?.showInfoWindow()
     }
 
-    /*
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+
+/*
     private fun getAddress(lat: Double, lon: Double): String? {
         val geoCoder = Geocoder(this, Locale.getDefault())
         val addresses = geoCoder.getFromLocation(lat, lon, 1)
         return addresses[0].getAddressLine(0)
     }
 
-     */
+ */
+
+
 }
